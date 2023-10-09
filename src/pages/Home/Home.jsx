6815e7bbdeee1b9595b30e36fd57ab17cd62060b
altamiro <altamiroribeirodarocha@gmail.com>
2023-10-09 15:12:50 -0300

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import "./Home.sass";
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>
+        React + <span>SASS</span>
+      </h1>
+      <a href="https://www.instagram.com/junior.rx22/" target="blank">
+        {"$"}
+        <span>{"{"}</span> Hey Dev <span>{"}"}</span>
+      </a>
+    </div>
+  );
+};
+
+export default Home;
